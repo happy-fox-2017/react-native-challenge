49951fd389a721305438ec83efa44305a200809e
myyusuf <myyusuf1911@gmail.com>
2017-08-02 14:46:16 +0700

    Use third party api

--- myproject/components/PeopleList.js
+++ myproject/components/PeopleList.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import { FlatList, StyleSheet, Text } from 'react-native';
 
+import axios from 'axios';
+
+const SWAPI_URL = 'https://swapi.co/api/people';
+
 class PeopleList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: [],
+    };
+  }
+
+  componentWillMount() {
+    axios.get(SWAPI_URL)
+    .then((response) => {
+      console.log(response);
+      this.setState({
+        people: response.data.results,
+      });
+    });
+  }
 
   render() {
     return (
       <FlatList
-       data={[
-         { key: 'Devin' },
-         { key: 'Jackson' },
-         { key: 'James' },
-         { key: 'Joel' },
-         { key: 'John' },
-         { key: 'Jillian' },
-         { key: 'Jimmy' },
-         { key: 'Julie' },
-       ]}
-      renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        data={this.state.people}
+        renderItem={({ item }) => <Text style={ styles.item }>{item.name}</Text>}
       />
     );
   }
